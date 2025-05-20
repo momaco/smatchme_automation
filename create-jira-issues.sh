bc99e91ef7c6b69333dc3e4cdee092d1c5e05e79
momaco <normita.ireri@gmail.com>
2025-05-20 14:23:10 -0600

    modifying curl

--- create-jira-issues.sh
+++ create-jira-issues.sh
@@ -32,37 +32,21 @@ for file in allure-results/*result*.json; do
       -H "Content-Type: application/json" \
       -H "Accept: application/json" \
       -H "Cookie:"atlassian.xsrf.token=2d4c372aaef9037e48d7408f0d06bb0c4e7de2ba_lin" \
-     --data '{
-        "fields": {
-          "project": { 
-            "key": "$PROJECT_KEY" 
-            },
-          "summary": "$summary",
-          "reporter": {
-            "id": "712020:c070dc6e-c712-473e-8870-93aac1c5fd46"
-            },
-          "issuetype": {
-            "name": "Bug",
-            "id": "10006"
-             },
-          "labels": ["auto-test-failed"],
-          "description": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "$description",
-              "type": "text"
-            }
-          ],
-          "type": "paragraph"
-        }
-      ],
-      "type": "doc",
-      "version": 1
-    }
-        }
-}'
+     --data "{
+        \"fields\": {
+          \"project\": { \"key\": \"$PROJECT_KEY\"},
+          \"summary\": \"$summary\",
+          \"reporter\": {\"id\": \"712020:c070dc6e-c712-473e-8870-93aac1c5fd46\"},
+          \"issuetype\": {\"name\": \"Bug\",\"id\": \"10006\"},
+          \"labels\": [\"auto-test-failed\"],
+          \"description\": {
+      \"content\": [{
+          \"content\": [
+            {\"text\": \"$description\",\"type\": \"text\"}],
+          \"type\": \"paragraph\"}],
+      \"type\": \"doc\",
+      \"version\": 1}
+      }}"
  "$JIRA_URL/rest/api/3/issue")
 
     if [ "$response" = "201" ]; then
